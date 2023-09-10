148e1e529dfe2977805ddf9153c10b591f9b0a2e
Sonu Kumar <sonukrsingh998@gmail.com>
2023-09-10 19:14:16 +0530

    complete till 8th sept

--- library-management-system/src/main/java/com/example/librarymanagementsystem/StudentController.java
+++ library-management-system/src/main/java/com/example/librarymanagementsystem/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("/student")
@@ -25,10 +26,33 @@ public class StudentController {
         return new ResponseEntity(student,HttpStatus.FOUND);
     }
     // delete a student --> regNo
+    @DeleteMapping("/delete")
+    public ResponseEntity deleteStudent(@RequestParam("id") int regNo){
+        return studentService.deleteStudent(regNo);
+    }
 
     // update the age of a student  ---> regNo, age
-
+    @PutMapping("/update-age/{age}")
+    public ResponseEntity updateAge(@RequestParam("id") int regNo, @PathVariable("age") int age){
+        Student student=studentService.updateAge(regNo,age);
+        if(student!=null) return new ResponseEntity(student,HttpStatus.FOUND);
+        return new ResponseEntity("invalid id",HttpStatus.NOT_FOUND);
+    }
     // get all the students in the db
-
+    @GetMapping("/all-student")
+    public ResponseEntity getAllStudent(){
+        List<Student> studentList=studentService.getAllStudent();
+        if(studentList.size()!=0)
+            return new ResponseEntity<>(studentList,HttpStatus.FOUND);
+        return new ResponseEntity<>("No student found in database",HttpStatus.NOT_FOUND);
+    }
     // get list of all male students
+    @GetMapping("/all-male-student")
+    public ResponseEntity getAllMaleStudent(){
+        List<Student> studentList=studentService.getAllMaleStudent();
+        if(studentList.size()!=0)
+            return new ResponseEntity<>(studentList,HttpStatus.FOUND);
+        return
+                new ResponseEntity<>("No male student found",HttpStatus.NOT_FOUND);
+    }
 }
--- library-management-system/src/main/java/com/example/librarymanagementsystem/StudentService.java
+++ library-management-system/src/main/java/com/example/librarymanagementsystem/StudentService.java
@@ -1,8 +1,12 @@
 package com.example.librarymanagementsystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +23,40 @@ public class StudentService {
         if(student.isPresent())
             return student.get();
         return null;
+    }
+
+    public ResponseEntity deleteStudent(int regNo) {
+        if(getStudent(regNo)!=null){
+             studentRepository.deleteById(regNo);
+             return new ResponseEntity<>("student deleted successfully",HttpStatus.FOUND);
+        }
+        return new ResponseEntity("invalid id", HttpStatus.NOT_FOUND);
+    }
+
+    public Student updateAge(int regNo, int age) {
+        if(getStudent(regNo)!=null) {
+            Student student = studentRepository.getById(regNo);
+            student.setAge(age);
+            addStudent(new Student(student.getRegNo(),student.getGender(),student.getName(),student.getEmail(),student.getAge()));
+            return student;
+        }
+        return null;
+    }
+
+    public List<Student> getAllStudent() {
+        List<Student> studentList=studentRepository.findAll();
+        return studentList;
+    }
+
+    public List<Student> getAllMaleStudent() {
+        List<Student> studentList =getAllStudent();
+        List<Student>ans=new ArrayList<>();
+        for(Student student:studentList){
+            if(student.getGender().equals(Gender.MALE)){
+                ans.add(student);
+            }
+        }
+        return ans;
+
     }
 }
